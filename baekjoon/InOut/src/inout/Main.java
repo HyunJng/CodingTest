ed8f74fa7774fee8668fbd30cec9f276b7b8baef
KHJ <khjung1654@gmail.com>
2022-10-18 21:35:00 +0900

    입출력 연습 : 백준 1000

---
+++ baekjoon/InOut/src/inout/Main.java
@@ -0,0 +1,15 @@
+package inout;
+//https://rlakuku-program.tistory.com/m/33
+// https://nahwasa.com/m/172
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int i = Integer.parseInt(br.readLine());
+				
+		
+	}
+}
---
+++ baekjoon/InOut/src/inout/Q1000.java
@@ -0,0 +1,14 @@
+package inout;
+
+import java.util.Scanner;
+
+// A + B
+public class Q1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.printf("%d");
+	}
+}
